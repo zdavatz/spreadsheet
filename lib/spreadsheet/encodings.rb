491e096bf05f78decaa63331a927d23ba8d4fe05
Hannes Wyss <hwyss@ywesee.com>
2008-10-24 10:02:47 +0200

    Remove obsolete code that triggers Iconv::InvalidEncoding on system with non-gnu Iconv

--- lib/spreadsheet/encodings.rb
+++ lib/spreadsheet/encodings.rb
@@ -3,9 +3,6 @@ module Spreadsheet
   # Methods for Encoding-conversions. You should not need to use any of these.
   module Encodings
     if RUBY_VERSION >= '1.9'
-      def ascii string
-        string.encode 'ASCII//TRANSLIT//IGNORE'
-      end
       def client string, internal='UTF-16LE'
         string.encode Spreadsheet.client_encoding
       end
@@ -14,15 +11,7 @@ module Spreadsheet
       end
     else
       require 'iconv'
-      @@utf8_utf16 = Iconv.new('UTF-16LE', 'UTF-8')
-      @@utf16_ascii = Iconv.new('ASCII//TRANSLIT//IGNORE', 'UTF-16LE')
-      @@utf16_utf8 = Iconv.new('UTF-8//TRANSLIT//IGNORE', 'UTF-16LE')
       @@iconvs = {}
-      def ascii string
-        @@utf16_ascii.iconv string
-      rescue
-        string.gsub /[^\x20-\x7e]+/, ''
-      end
       def client string, internal='UTF-16LE'
         key = [Spreadsheet.client_encoding, internal]
         iconv = @@iconvs[key] ||= Iconv.new(Spreadsheet.client_encoding, internal)
@@ -36,9 +25,6 @@ module Spreadsheet
     end
   rescue LoadError
     warn "You don't have Iconv support compiled in your Ruby. Spreadsheet may not work as expected"
-    def ascii string
-      string.gsub /[^\x20-\x7e]+/, ''
-    end
     def client string, internal='UTF-16LE'
       string.delete "\0"
     end
