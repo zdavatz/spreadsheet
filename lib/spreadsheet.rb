52c1c383a04ff719d5abb18ea0006d4715cca593
Hannes Wyss <hwyss@ywesee.com>
2009-07-03 20:37:26 +0200

    Release 0.6.4

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -42,7 +42,7 @@ module Spreadsheet
 
   ##
   # The version of Spreadsheet you are using.
-  VERSION = '0.6.3.1'
+  VERSION = '0.6.4'
 
   ##
   # Default client Encoding. Change this value if your application uses a
