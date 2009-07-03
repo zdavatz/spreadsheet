zdavatz/spreadsheet

commit 040466c847b83b6c6e0cd67800499ce899c59c30

Leverage DateTime#+ when reading Dates.
Fixes a Bug reported by Iwan Buetti in:
http://rubyforge.org/tracker/index.php?func=detail&aid=24414&group_id=678&atid=2677

lib/spreadsheet/excel/row.rb
@@ -45,31 +45,16 @@ class Row < Spreadsheet::Row
   private
   def _date data # :nodoc:
     return data if data.is_a?(Date)
-    date = @worksheet.date_base + data.to_i
+    datetime = _datetime data
+    Date.new datetime.year, datetime.month, datetime.day
+  end
+  def _datetime data # :nodoc:
+    return data if data.is_a?(DateTime)
+    date = @worksheet.date_base + data.to_f
     if date > LEAP_ERROR
       date -= 1
     end
     date
-  end
-  def _datetime data # :nodoc:
-    return data if data.is_a?(DateTime)
-    date = _date data
-    hour = (data % 1) * 24
-    min  = (hour % 1) * 60
-    sec  = ((min  % 1) * 60).round
-    min = min.floor
-    hour = hour.floor
-    if sec > 59
-      sec = 0
-      min += 1
-    end
-    if min > 59
-      hour += 1
-    end
-    if hour > 23
-      date += 1
-    end
-    DateTime.new(date.year, date.month, date.day, hour, min, sec)
   end
   def enriched_data idx, data # :nodoc:
     res = nil

test/integration.rb
@@ -1209,5 +1209,30 @@ module Spreadsheet
       assert_equal -bigfloat + 0.1, sheet[1,1]
       assert_equal -bigfloat - 0.1, sheet[1,2]
     end
+    def test_datetime
+      # reported in http://rubyforge.org/tracker/index.php?func=detail&aid=24414&group_id=678&atid=2677
+      datetime1 = DateTime.new(2008)
+      datetime2 = DateTime.new(2008, 1, 1, 1, 0, 1)
+      date1 = Date.new(2008)
+      date2 = Date.new(2009)
+      book = Spreadsheet::Workbook.new
+      sheet = book.create_worksheet
+      sheet[0,0] = datetime1
+      sheet[0,1] = datetime2
+      sheet[1,0] = date1
+      sheet[1,1] = date2
+      path = File.join @var, 'test_datetime.xls'
+      book.write path
+      assert_nothing_raised do
+        book = Spreadsheet.open path
+      end
+      sheet = book.worksheet(0)
+      assert_equal datetime1, sheet[0,0]
+      assert_equal datetime2, sheet[0,1]
+      assert_equal date1, sheet[1,0]
+      assert_equal date2, sheet[1,1]
+      assert_equal date1, sheet.row(0).date(0)
+      assert_equal datetime1, sheet.row(1).datetime(0)
+    end
   end
 end

lib/spreadsheet/excel/reader.rb
@@ -770,9 +770,9 @@ class Reader
       when :datemode   # ○  DATEMODE ➜ 6.25
         flag, _ = work.unpack 'v'
         if flag == 1
-          @workbook.date_base = Date.new 1904, 1, 1
+          @workbook.date_base = DateTime.new 1904, 1, 1
         else
-          @workbook.date_base = Date.new 1899, 12, 31
+          @workbook.date_base = DateTime.new 1899, 12, 31
         end
       when :continue   # ○  CONTINUE ➜ 6.22
         case previous_op

lib/spreadsheet/excel/workbook.rb
@@ -61,7 +61,7 @@ class Workbook < Spreadsheet::Workbook
     end
   end
   def date_base
-    @date_base ||= Date.new 1899, 12, 31
+    @date_base ||= DateTime.new 1899, 12, 31
   end
   def shared_string idx
     @sst[idx.to_i].content

commit 09c47342e5e671233c3bcba5f6303c60854c0e6f

Fix a regression introduced in 040466c847b83b6c6e0cd67800499ce899c59c30
That previous commit introduced a new test with a previously defined
name - thus allowing a regression to creep in.

lib/spreadsheet/excel/row.rb
@@ -52,10 +52,25 @@ class Row < Spreadsheet::Row
     return data if data.is_a?(DateTime)
     base = @worksheet.date_base
     date = base + data.to_f
+    hour = (data % 1) * 24
+    min  = (hour % 1) * 60
+    sec  = ((min % 1) * 60).round
+    min = min.floor
+    hour = hour.floor
+    if sec > 59
+      sec = 0
+      min += 1
+    end
+    if min > 59
+      hour += 1
+    end
+    if hour > 23
+      date += 1
+    end
     if LEAP_ERROR > base
       date -= 1
     end
-    date
+    DateTime.new(date.year, date.month, date.day, hour, min, sec)
   end
   def enriched_data idx, data # :nodoc:
     res = nil

test/integration.rb
@@ -525,7 +525,7 @@ module Spreadsheet
       end
       assert_equal sheet, book.worksheet(str)
     end
-    def test_datetime
+    def test_read_datetime
       path = File.join @data, 'test_datetime.xls'
       book = Spreadsheet.open path
       assert_instance_of Excel::Workbook, book
@@ -604,9 +604,9 @@ module Spreadsheet
       assert_equal 12, sheet.column_count
       useds = [0,0,0,0,0,0,0,0,0,0,0]
       unuseds = [2,2,1,1,1,2,1,11,1,2,12]
-      sheet.each do |row|
-        assert_equal useds.shift, row.first_used
-        assert_equal unuseds.shift, row.first_unused
+      sheet.each do |rw|
+        assert_equal useds.shift, rw.first_used
+        assert_equal unuseds.shift, rw.first_unused
       end
       assert unuseds.empty?, "not all rows were visited in Spreadsheet#each"
       row = sheet.row 0
@@ -713,9 +713,9 @@ module Spreadsheet
       assert_equal 12, sheet.column_count
       useds = [0,0,0,0,0,0,0,0,0,0,0]
       unuseds = [2,2,1,1,1,2,1,11,1,2,12]
-      sheet.each do |row|
-        assert_equal useds.shift, row.first_used
-        assert_equal unuseds.shift, row.first_unused
+      sheet.each do |rw|
+        assert_equal useds.shift, rw.first_used
+        assert_equal unuseds.shift, rw.first_unused
       end
       assert unuseds.empty?, "not all rows were visited in Spreadsheet#each"
       row = sheet.row 0
@@ -1205,11 +1205,11 @@ module Spreadsheet
       assert_equal bigfloat, sheet[0,0]
       assert_equal bigfloat + 0.1, sheet[0,1]
       assert_equal bigfloat - 0.1, sheet[0,2]
-      assert_equal -bigfloat, sheet[1,0]
-      assert_equal -bigfloat + 0.1, sheet[1,1]
-      assert_equal -bigfloat - 0.1, sheet[1,2]
+      assert_equal(-bigfloat, sheet[1,0])
+      assert_equal(-bigfloat + 0.1, sheet[1,1])
+      assert_equal(-bigfloat - 0.1, sheet[1,2])
     end
-    def test_datetime
+    def test_datetime__off_by_one
       # reported in http://rubyforge.org/tracker/index.php?func=detail&aid=24414&group_id=678&atid=2677
       datetime1 = DateTime.new(2008)
       datetime2 = DateTime.new(2008, 1, 1, 1, 0, 1)