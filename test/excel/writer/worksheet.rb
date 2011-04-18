cafe539f0e20b46d52b65ab0ec157341ce1f7ec4
Masaomi Hatakeyama <mhatakeyama@ywesee.com>
2011-04-18 15:13:31 +0200

    Added a test for the patch from Bjoern Andersson regarding NaN (Not a Number)

--- test/excel/writer/worksheet.rb
+++ test/excel/writer/worksheet.rb
@@ -14,6 +14,7 @@ class TestWorksheet < Test::Unit::TestCase
     assert_equal false, sheet.need_number?(114.55)
     assert_equal false, sheet.need_number?(0.1)
     assert_equal false, sheet.need_number?(0.01)
+    assert_equal false, sheet.need_number?(0 / 0.0) # NaN
     assert_equal true, sheet.need_number?(0.001)
     assert_equal true, sheet.need_number?(10000000.0)
   end
