9bd6f7a3354a8da40ef7e260256a9472a59d09e4
Zeno R.R. Davatz <zdavatz@ywesee.com>
2011-09-07 17:19:59 +0200

    Updated History.txt and lib/spreadsheet.rb with the version 0.6.5.9

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -42,7 +42,7 @@ module Spreadsheet
 
   ##
   # The version of Spreadsheet you are using.
-  VERSION = '0.6.5.8'
+  VERSION = '0.6.5.9'
 
   ##
   # Default client Encoding. Change this value if your application uses a
