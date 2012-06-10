zdavatz/spreadsheet

commit 175523d6879353596310bedaefb06d12fe35049c

Speed up writing: Improve #strings performance
 #strings now returns a Hash representing the string frequency:
  "bob" => 1,
  "mary" => 50

 This is significantly more efficient than arrays, especially for
 cases where content repeats in multiple cells.  Further operations such
 as counting uniques, and set subtraction, are also more efficient.

 Given a simple test program on my laptop:

  book = Spreadsheet::Workbook.new
  sheet = book.create_worksheet(:name => "Foo")
  (1..15000).each do |i|
    row = sheet.row(i)
    row.concat(%w(hello hello hello hello hello hello hello hello hello hello hello hello))
  end
  book.write("book.xls")

Runtime before change:
  real  0m37.723s

Runtime after change:
  real  0m11.922s

lib/spreadsheet/excel/writer/workbook.rb
@@ -71,19 +71,21 @@ class Workbook < Spreadsheet::Writer
   end
   def complete_sst_update? workbook
     stored = workbook.sst.collect do |entry| entry.content end
-    current = worksheets(workbook).inject [] do |memo, worksheet|
-      memo.concat worksheet.strings
+    current = worksheets(workbook).inject(Hash.new(0)) do |memo, worksheet|
+      worksheet.strings.each do |k,v|
+        memo[k] += v
+      end
+      memo
     end
-    total = current.size
-    current.uniq!
+    num_total = current.values.inject(0){|memo,v| memo+v}
     current.delete ''
-    if (stored - current).empty? && !stored.empty?
+    if !stored.empty? && stored.all?{|x| current[x]}
       ## if all previously stored strings are still needed, we don't have to
       #  rewrite all cells because the sst-index of such string does not change.
-      additions = current - stored
-      [:partial_update, total, stored + additions]
+      additions = current.keys - stored
+      [:partial_update, num_total, stored + additions]
     else
-      [:complete_update, total, current]
+      [:complete_update, num_total, current.keys]
     end
   end
   def font_index workbook, font_key
@@ -482,12 +484,14 @@ class Workbook < Spreadsheet::Writer
     #      0     4  Total number of strings in the workbook (see below)
     #      4     4  Number of following strings (nm)
     #      8  var.  List of nm Unicode strings, 16-bit string length (➜ 3.4)
-    strings = worksheets(workbook).inject [] do |memo, worksheet|
-      memo.concat worksheet.strings
+    strings = worksheets(workbook).inject(Hash.new(0)) do |memo, worksheet|
+      worksheet.strings.each do |k,v|
+        memo[k] += v
+      end
+      memo
     end
-    total = strings.size
-    strings.uniq!
-    _write_sst workbook, writer, offset, total, strings
+    num_total = strings.values.inject(0){|memo,v| memo+v}
+    _write_sst workbook, writer, offset, num_total, strings.keys
   end
   def _write_sst workbook, writer, offset, total, strings
     sst = {}

lib/spreadsheet/excel/writer/n_worksheet.rb
@@ -118,9 +118,11 @@ class Worksheet
     @io.size
   end
   def strings
-    @worksheet.inject [] do |memo, row|
-      strings = row.select do |cell| cell.is_a?(String) && !cell.empty? end
-      memo.concat strings
+    @worksheet.inject(Hash.new(0)) do |memo, row|
+      row.each do |cell|
+        memo[cell] += 1 if (cell.is_a?(String) && !cell.empty?)
+      end
+      memo
     end
   end
   ##

lib/spreadsheet/excel/writer/worksheet.rb
@@ -118,9 +118,11 @@ class Worksheet
     @io.size
   end
   def strings
-    @worksheet.inject [] do |memo, row|
-      strings = row.select do |cell| cell.is_a?(String) && !cell.empty? end
-      memo.concat strings
+    @worksheet.inject(Hash.new(0)) do |memo, row|
+      row.each do |cell|
+        memo[cell] += 1 if (cell.is_a?(String) && !cell.empty?)
+      end
+      memo
     end
   end
   ##

commit 72a525eaa2d9a2ea45e3c8b4c71f152a434c9cb2

Bugfix: Proper SST partial vs. complete writing
This was broken by 175523d6879353596310bedaefb06d12fe35049c - the bug
caused "partial" to always be chosen.

The net effect was slightly larger SST tables, nothing more...

lib/spreadsheet/excel/writer/workbook.rb
@@ -83,7 +83,7 @@ class Workbook < Spreadsheet::Writer
     end
     num_total = current.values.inject(0){|memo,v| memo+v}
     current.delete ''
-    if !stored.empty? && stored.all?{|x| current[x]}
+    if !stored.empty? && stored.all?{|x| current.include?(x) }
       ## if all previously stored strings are still needed, we don't have to
       #  rewrite all cells because the sst-index of such string does not change.
       additions = current.keys - stored