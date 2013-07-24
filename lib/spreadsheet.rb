7600b5d50f8b51856800204410e85cfaf5ee5b54
Zeno R.R. Davatz <zdavatz@ywesee.com>
2013-07-24 12:25:13 +0200

    spreadsheet-0.8.7.gem released - should be ready for Ruby 2.0.0

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -43,7 +43,7 @@ module Spreadsheet
 
   ##
   # The version of Spreadsheet you are using.
-  VERSION = '0.8.6'
+  VERSION = '0.8.7'
 
   ##
   # Default client Encoding. Change this value if your application uses a
