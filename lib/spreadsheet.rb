94feb3104e0cda089f91eef12d6b4492e36fba3a
Zeno R.R. Davatz <zdavatz@ywesee.com>
2013-08-02 13:22:07 +0200

    spreadsheet-0.8.8.gem released

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -43,7 +43,7 @@ module Spreadsheet
 
   ##
   # The version of Spreadsheet you are using.
-  VERSION = '0.8.7'
+  VERSION = '0.8.8'
 
   ##
   # Default client Encoding. Change this value if your application uses a
