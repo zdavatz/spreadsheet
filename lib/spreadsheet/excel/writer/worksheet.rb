386181ccc635399690b6f50a027def710ace77fc
cantin <cantin2010@gmail.com>
2015-03-05 16:13:59 +0800

    add rational requirement

--- lib/spreadsheet/excel/writer/worksheet.rb
+++ lib/spreadsheet/excel/writer/worksheet.rb
@@ -3,6 +3,7 @@ require 'spreadsheet/excel/writer/biff8'
 require 'spreadsheet/excel/internals'
 require 'spreadsheet/excel/internals/biff8'
 require 'bigdecimal'
+require 'rational' #for Ruby 1.8.x
 
 module Spreadsheet
   module Excel
