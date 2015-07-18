61881f4f257bd86c00c23eec6836179d425f0514
Zeno R.R. Davatz <zdavatz@ywesee.com>
2015-07-18 09:40:22 +0200

    updated lib/spreadsheet.rb

--- lib/spreadsheet.rb
+++ lib/spreadsheet.rb
@@ -45,7 +45,7 @@ module Spreadsheet
 
   ##
   # The version of Spreadsheet you are using.
-  VERSION = '1.0.3'
+  VERSION = '1.0.4'
 
   ##
   # Default client Encoding. Change this value if your application uses a
